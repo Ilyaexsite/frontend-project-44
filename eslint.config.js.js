337ef3ed219d4b823d79230c729487184171b106
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-07-05 17:06:40 +0300

    new

--- eslint.config.js.js
+++ eslint.config.js.js
@@ -1,18 +1,21 @@
+import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default [
+  js.configs.recommended,
   {
     plugins: {
-      '@stylistic': stylistic,
+      '@stylistic': stylistic
     },
     rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+
       '@stylistic/indent': ['error', 2],
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/semi': ['error', 'always'],
       '@stylistic/brace-style': ['error', '1tbs'],
-
-      'no-console': 'warn',
-      'no-unused-vars': 'error',
-    },
-  },
+      '@stylistic/comma-dangle': ['error', 'never']
+    }
+  }
 ];
