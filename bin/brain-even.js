05e8649b26df6f70d7d6d0f9c7232b20554f83b2
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-07-13 15:56:10 +0300

    new

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,9 +2,11 @@
 
 import { greetUser, getUserName } from '../src/cli.js';
 import { getRandomInt, isEven } from '../src/gameUtils.js';
+
 greetUser();
 const userName = getUserName();
 console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
 const number = getRandomInt(1, 100);
-console.log(`Question: ${number}`);
+console.log(`Question: ${number}`); 
--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,8 @@ import readlineSync from 'readline-sync';
 
 export function greetUser() {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+}
+
+export function getUserName() {
+  return readlineSync.question('May I have your name? ');
 }
