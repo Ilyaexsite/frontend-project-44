373f3d0378ad0be55cba9e1e42eac9aa68a233b8
Ilyaexsite <ilyatsarygin64@gmail.com>
2025-07-13 14:16:48 +0300

    new nod

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
+
 export const getRandomInt = (min, max) => {
-    const minInt = Math.ceil(min);
-    const maxInt = Math.floor(max);
-    return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
-  };
+  const minInt = Math.ceil(min);
+  const maxInt = Math.floor(max);
+  return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
+};
+
+export const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
